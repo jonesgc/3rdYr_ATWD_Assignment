53c9aea16986d79838bea583e0238ba26809870c
greg <gregcj50@gmail.com>
2018-09-28 15:18:22 +0100

    Started constructing XML document for currencies
    The XML document will be holding the value of the currencies, this will be queried then the data passed through the API to the client.

---
+++ curDataMonitor.php
@@ -0,0 +1,8 @@
+<?
+//Author: Gregory Jones
+//This script will be run once every few hours, it will check the curData.xml file against the infomation pulled from openexchangerates.
+//If there is a difference between the data then the script will update curData with the newer data. 
+
+echo "hello";
+
+?>
