ecb00e64577b82328325c6dcb9aaac6ee36cfa3b
greg <gregcj50@gmail.com>
2018-09-30 15:58:54 +0100

    Begun implementation of function to get Api data
    The function to get the api data and update the xml document has been started, it currently gets the data and decodes the json response into an associative array.

--- curDataMonitor.php
+++ curDataMonitor.php
@@ -3,6 +3,8 @@
 //This script will be run once every few hours, it will check the curData.xml file against the infomation pulled from openexchangerates.
 //If there is a difference between the data then the script will update curData with the newer data. 
 
+$id = '08c94e1539cd46c69ef98a7a2a94ca7a';
+
 if (file_exists('curData.xml'))
 {
     $xml = simplexml_load_file('curData.xml');
@@ -10,8 +12,6 @@ if (file_exists('curData.xml'))
 else{
     echo "Cant find currency data file.";
 }
-
-
 
 
 function updateCurDataAcessed ($xml)
@@ -28,13 +28,27 @@ function updateCurDataAcessed ($xml)
     {
         $xml->updated['time'] = $servTime;
         $xml->updated['date'] = $servDate;
-        echo $time;
+        //echo $time->asxml();
         file_put_contents('curData.xml', $xml->asxml());
     }
 }
 
+function updateCurData ($xml, $id)
+{
+    //Checks if there are differences between the data in the xml file and the data in the currency feed.
+    //Using USD as the base currency, hence why USD is 1. 
+    $latestURL = "https://openexchangerates.org/api/latest.json?app_id=" . $id;
+    $latestData = json_decode(file_get_contents($latestURL), true);
+    print_r($latestData);
+    foreach ($xml->rates->cur->rate as $cur) 
+    {
+        echo $cur;
+    }
+}
+
+//echo $xml->rates->USD;
 //header('Content-Type: text/xml');
 //echo $xml;
-
+updateCurData($xml, $id);
 updateCurDataAcessed($xml);
 ?>
