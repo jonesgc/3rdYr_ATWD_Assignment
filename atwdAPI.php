7dcbc3955216cc719d5be6b3d7192e5c86f11475
greg <gregcj50@gmail.com>
2018-10-05 19:24:54 +0100

    converter function created
    Currently using hard coded values, these need to be replaced by API call args. Unsure of the accuracy of the conversion, might need to revise the method chosen.

--- atwdAPI.php
+++ atwdAPI.php
@@ -1,4 +1,50 @@
 <?php
+
 $method = $_SERVER['REQUEST_METHOD'];
 
+//Get the exchange rates xml file for use in conversions.
+if (file_exists('curData.xml'))
+{
+    $xml = simplexml_load_file('curData.xml');
+}
+else
+{
+    echo "Cant find currency data file.";
+}
+
+
+function convertCur($base, $origin, $target, $amount, $xml)
+{
+    //This function converts between the origin currency and the target currency using the base currency as an intermedary.
+    //Current solution O-->B-->T
+    
+    //Get the rates for origin and target vs the base currency.
+    foreach ($xml->rates->cur as $currency) 
+    {
+        $name = (string)$currency->name;
+        $rate = (string)$currency->rate;
+        
+        if($name == $base)
+        {
+            //Cannot alter the base currency in this function.
+        }
+        elseif($name == $origin)
+        {
+            $originVal = $rate;
+        }
+        elseif($name == $target)
+        {
+            $targetVal = $rate;
+        }
+    }
+
+    $result = ($amount * $originVal)/$targetVal;
+
+    return $result;
+}
+$base = 'GBP';
+$origin = 'DKK';
+$amount = 1;
+$target = 'JPY';
+echo convertCur($base, $origin, $target, $amount,$xml);
 ?>
