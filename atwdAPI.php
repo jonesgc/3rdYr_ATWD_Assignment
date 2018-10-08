971a0f93525156b4fdbed99651d52027d00915f0
greg <gregcj50@gmail.com>
2018-10-08 17:13:21 +0100

    Get now works
    The get response can now return xml, however there seems to be a problem in what is returned. The input values are input into an (with tags) unstructured string which is printed at the end of the structured but blank xml file.

--- atwdAPI.php
+++ atwdAPI.php
@@ -2,6 +2,7 @@
 
 $method = $_SERVER['REQUEST_METHOD'];
 $query = $_SERVER['QUERY_STRING'];
+$base = 'GBP';
 
 //Get the exchange rates xml file for use in conversions.
 if (file_exists('curData.xml'))
@@ -15,12 +16,12 @@ else
 
 
 //Swtitch on the method in the request then call the corisponding function.
-function methodController($method, $query)
+function methodController($method, $query, $base, $xml)
 {
     
     switch ($method)
     {
-        case 'GET': respondGET($query);break;
+        case 'GET': respondGET($query, $base, $xml);break;
         case 'PUT':break;
         case 'POST':break;
         case 'DELETE':break;
@@ -33,6 +34,8 @@ function methodController($method, $query)
 function convertCur($base, $origin, $target, $amount, $xml)
 {
     
+    $originVal = 0;
+    $targetVal = 0;
     
     //Get the rates for origin and target vs the base currency.
     foreach ($xml->rates->cur as $currency) 
@@ -54,23 +57,50 @@ function convertCur($base, $origin, $target, $amount, $xml)
         }
     }
 
-    $result = ($amount * $originVal)/$targetVal;
+    //Perform the conversion, using the base currency as a "stepping stone"
+    $newAmount = ($amount * $originVal)/$targetVal;
 
-    return $result;
+    //Find the get response XML template.
+    if (file_exists('curData.xml')) 
+    {
+        $result = simplexml_load_file('getRes.xml');
+
+        $dt = date('d/m/y\ m:h');
+        $result->conv->at = $dt;
+
+        //Origin or from return values input into response xml.
+        $result->conv->from->code = $origin;
+        $result->conv->from->rate = $originVal;
+        //Missing location data!
+        $result->conv->from->amnt = $amount;
+        
+        //Target or to values input into response xml.
+        $result->conv->to->code = $target;
+        $result->conv->to->amnt = $newAmount;
+        return $result;
+    }
+    else
+    {
+        //Error
+        echo "Cant find get response template";
+        return "Error";
+    }
 }
 
 //Respond to a GET request.
 //Expected response type is XML.
-function respondGET ($query)
+function respondGET ($query, $base, $xml)
 {
-    print_r(explode('&', $query));
-    echo $query;
+    $params = (explode('&', $query));
+    $origin = $params[0];
+    $target = $params[1];
+    $amount = $params[2];
+    $type = $params[3];
+    $result = convertCur($base, $origin, $target, $amount, $xml);
+    echo $result->asXML();
 }
 
-$base = 'GBP';
-$origin = 'DKK';
-$amount = 1;
-$target = 'JPY';
 //echo convertCur($base, $origin, $target, $amount,$xml);
-methodController($method, $query);
+methodController($method, $query, $base, $xml);
+
 ?>
