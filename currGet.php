8eeb6888578aa918ea64a99c2737df42346f7b0c
greg <gregcj50@gmail.com>
2018-10-22 21:25:49 +0100

    Added more error handling to GET
    Added the other error possible error cases to get. Function should now handle all errors required by spec, further testing needed (from url as well)

--- currGet.php
+++ currGet.php
@@ -16,16 +16,8 @@ if (file_exists('config.xml'))
 }
 else
 {
-    $err = 1500;
-    generateError($err,"XML");
+    generateError(1500,"XML");
 }
-
-
-
-
-
-
-
 
 //----FUNCTIONS----
 
@@ -119,85 +111,136 @@ function convertCur($base, $origin, $target, $amount, $xml)
 //Base is declared in config.php. 
 function respondGET ($query, $base, $xml)
 {
+
+    //Extract variables from query string, implemented for readability.
     $params = (explode('&', $query));
+
+    //Check if query string is empty.
+    if($params)
+    {
+        generateError(1000, "XML");
+    }
+
     $origin = $params[0];
     $target = $params[1];
     $amount = $params[2];
     $type = $params[3];
-    $result = convertCur($base, $origin, $target, $amount, $xml);
 
-     //Get the data for the response from XML file.
-     $oDat = findData($origin, $xml);
-     $oCurrName = $oDat['name'];
-     $oLocs = $oDat['loc'];
+    //Validate parameters, checking if codes do not contain numbers and are all caps, and numbers are decimals.
+    $oTest = preg_match('/([A-Z])([^a-z])/', $origin);
+    $tTest = preg_match('/([A-Z])([^a-z])/', $target);
 
-     $tDat = findData($target, $xml);
-     $tCurrName = $tDat['name'];
-     $tLocs = $tDat['loc'];
-
-	//Catch an error in currency codes being wrong.
-	if($result[0] == "ERROR")
-	{
-        //Throw an error depending on what cause the convert cur function to error.
-        $err = $result[1];
-        generateError($err, $type);
-	}
-    elseif($type == 'XML')
+    //These tests check if the input parameter is the correct style and exists.
+    if(!$oTest)
     {
-        //Find the get response XML template.
-        if (file_exists('curData.xml'))
+        if($origin == "")
         {
-            $res = simplexml_load_file('getResXML.xml');
-
-            $res->conv->at = date("d/m/y \ h:i", (int)$xml->updated->dataUpdated);
-            
-
-            //Origin or from return values input into response xml.
-            $res->from->code = $origin;
-            $res->from->curr = $oCurrName;
-            $res->from->rate = $result[1];
-            $res->from->loc = $oLocs;
-            $res->from->amnt = $amount;
-            //Target or to values input into response xml.
-            $res->to->code = $target;
-            $res->to->curr = $tCurrName;
-            $res->to->amnt = $result[5];
-            $res->to->loc = $tLocs;
-
-			//Send response to server.
-            echo $res->asxml();
+            generateError(1000,"XML");
         }
         else
         {
-            //Error
-            echo "Cant find get response template";
+            generateError(1100, "XML");
         }
     }
-    elseif($type == 'JSON')
+    elseif(!$tTest)
     {
-		//Need to insert try catch.
-        $res = json_decode(file_get_contents('getResJSON.json'), true);
-
-        $res['conv']['at'] = date("d/m/y \ h:i", (int)$xml->updated->dataUpdated);
-        $res['conv']['rate'] = $result[1];
-
-        //Input origin or from response values into JSON.
-        $res['conv']['from']['code'] = $origin;
-        $res['conv']['from']['curr'] = $oCurrName;
-        $res['conv']['from']['loc'] = $oLocs;
-        $res['conv']['from']['amnt' ]= $amount;
-
-        //Input target or to response values into JSON.
-        $res['conv']['to']['code'] = $target;
-        $res['conv']['to']['curr'] = $tCurrName;
-        $res['conv']['to']['loc'] = $tLocs;
-        $res['conv']['to']['amnt'] = $result[5];
-
-        $res = json_encode($res);
-
-		//Send response to client.
-        echo $res;
+        if($target == "")
+        {
+            generateError(1000,"XML");
+        }
+        else
+        {
+            generateError(1100, "XML");
+        }
     }
+    //Check if either of the other parameters are missing.
+    elseif(($amount = "") || ($type = ""))
+    {
+        generateError(1000, "XML");
+    } 
+    //Check response type, must match either XML or JSON.
+    elseif(($type != 'XML') || ($type != 'JSON'))
+    {
+        generateError(1400,"XML");
+    }
+    //Continue function execution.
+    else
+    {
+        $result = convertCur($base, $origin, $target, $amount, $xml);
+
+        //Get the data for the response from XML file.
+        $oDat = findData($origin, $xml);
+        $oCurrName = $oDat['name'];
+        $oLocs = $oDat['loc'];
+   
+        $tDat = findData($target, $xml);
+        $tCurrName = $tDat['name'];
+        $tLocs = $tDat['loc'];
+        
+        //Catch an error in currency codes being wrong.
+        if($result[0] == "ERROR")
+        {
+            $err = $result[1];
+            generateError($err, $type);
+        }
+        elseif($type == 'XML')
+        {
+           //Find the get response XML template.
+           if (file_exists('curData.xml'))
+           {
+               $res = simplexml_load_file('getResXML.xml');
+   
+               $res->conv->at = date("d/m/y \ h:i", (int)$xml->updated->dataUpdated);
+               
+   
+               //Origin or from return values input into response xml.
+               $res->from->code = $origin;
+               $res->from->curr = $oCurrName;
+               $res->from->rate = $result[1];
+               $res->from->loc = $oLocs;
+               $res->from->amnt = $amount;
+               //Target or to values input into response xml.
+               $res->to->code = $target;
+               $res->to->curr = $tCurrName;
+               $res->to->amnt = $result[5];
+               $res->to->loc = $tLocs;
+   
+               //Send response to server.
+               echo $res->asxml();
+           }
+           else
+           {
+               //Error
+               echo "Cant find get response template";
+           }
+       }
+       elseif($type == 'JSON')
+       {
+           //Need to insert try catch.
+           $res = json_decode(file_get_contents('getResJSON.json'), true);
+   
+           $res['conv']['at'] = date("d/m/y \ h:i", (int)$xml->updated->dataUpdated);
+           $res['conv']['rate'] = $result[1];
+   
+           //Input origin or from response values into JSON.
+           $res['conv']['from']['code'] = $origin;
+           $res['conv']['from']['curr'] = $oCurrName;
+           $res['conv']['from']['loc'] = $oLocs;
+           $res['conv']['from']['amnt' ]= $amount;
+   
+           //Input target or to response values into JSON.
+           $res['conv']['to']['code'] = $target;
+           $res['conv']['to']['curr'] = $tCurrName;
+           $res['conv']['to']['loc'] = $tLocs;
+           $res['conv']['to']['amnt'] = $result[5];
+   
+           $res = json_encode($res);
+   
+           //Send response to client.
+           echo $res;
+       }
+    }
+   
 
 }
 ?>
